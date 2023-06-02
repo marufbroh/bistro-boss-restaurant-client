fa1543def2e5c3c36ca8a0eb853e1cf937cba259
marufbroh <marufmd775@gmail.com>
2023-06-02 18:50:39 +0600

    working on my cart page

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome } from 'react-icons/fa';
 
 const Dashboard = () => {
     return (
-        <div className="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col items-center justify-center">
-                {/* Page content here */}
-                <Outlet />
-                <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        <div className='container mx-auto border'>
+            <div className="drawer drawer-mobile">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex flex-col items-center justify-center">
+                    {/* Page content here */}
+                    <Outlet />
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 h-full bg-base-100 text-base-content">
+                        {/* Sidebar content here */}
+                        <li><Link><FaHome />User Home</Link></li>
+                        <li><Link><FaCalendarAlt />Reservation</Link></li>
+                        <li><Link><FaWallet />Payment History</Link></li>
+                        <li><Link to={"/dashboard/mycart"}><FaShoppingCart />My Cart</Link></li>
 
-            </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-100 text-base-content">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
+                        <div className="divider"></div>
+
+                        <li><Link to={"/"}><FaHome />Home</Link></li>
+                        <li><Link to={"/menu"}>Menu</Link></li>
+                        <li><Link to={"/order/salad"}>Order</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
--- src/pages/Dashboard/MyCart.jsx
+++ src/pages/Dashboard/MyCart.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import useCart from '../../hooks/useCart';
+import SectionTitle from '../../components/SectionTitle';
+import { FaTrashAlt } from 'react-icons/fa';
 
 const MyCart = () => {
+    const [cart] = useCart();
+    console.log(cart);
+    const total = cart.reduce((sum, item) => item.price + sum, 0)
     return (
         <div>
-            My cart page
+            <Helmet>
+                <title>Bistro Boss | My Cart</title>
+            </Helmet>
+            {/* <SectionTitle subHeading={"---My Cart---"} heading={"WANNA ADD MORE?"} /> */}
+            <div className='flex justify-evenly font-semibold uppercase items-center mb-3'>
+                <h3 className='text-3xl'>Total Oders: {cart.length}</h3>
+                <h3 className='text-3xl'>Total Price: ${total}</h3>
+                <button className='btn btn-warning btn-sm'>Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Item Image</th>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((item, index) => <tr key={item._id}>
+                                <td>{index + 1}</td>
+                                <td>
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={item.image} alt="" />
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{item.name}</td>
+                                <td className='text-end'>{item.price}</td>
+                                <td>
+                                    <button className="btn btn-warning btn-xs"><FaTrashAlt /></button>
+                                </td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
